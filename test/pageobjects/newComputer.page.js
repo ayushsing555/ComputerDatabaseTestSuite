bc1054faf73112891422da4f4cd4de2fe789274c
ayush singhal <ayushsinghalmains@gmail.com>
2023-04-12 23:44:56 +0530

    some modified

--- test/pageobjects/newComputer.page.js
+++ test/pageobjects/newComputer.page.js
@@ -1,32 +1,40 @@
 class newComputerPage {
+  // getting the inputComputer
   get inputComputerName() {
     return $("//input[@id='name']");
   }
 
+  // getting the introduced input
   get introduced() {
     return $("//input[@id='introduced']");
   }
 
+  // getting the discontinued input
   get discontinued() {
     return $("//input[@id='discontinued']");
   }
 
+  // getting selectBox
   get btnSelectBox() {
     return $("//select[@id='company']");
   }
 
+  // getting the option list
   get selectOptionList() {
     return $$("//select/option");
   }
 
+  // getting button of create computer
   get btnCreateComputer() {
     return $("//input[@value='Create this computer']");
   }
 
+  // getting the select box
   async selectBox() {
     await this.btnSelectBox.click();
   }
 
+  // functioning the create a  Computer
   async addComputer(name, introDate, discontinuedDate) {
     await this.inputComputerName.setValue(name);
     await this.introduced.setValue(introDate);
